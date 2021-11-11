1d9773d82c88fcd5bc0c5a6cf3c92246f6c7fc3a
dhkim1522 <dhkim1522@naver.com>
2021-11-12 02:29:54 +0900

    commit

---
+++ src/test/java/com/springboot/dhkim/dhkim/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.springboot.dhkim.dhkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(com.springboot.dhkim.Application.class, args);
+    }
+}
